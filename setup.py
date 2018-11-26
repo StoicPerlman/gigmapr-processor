544c231e14494f5e008418a0fec2af46b6e9f1c7
Sam Kleiner <sam@skleiner.com>
2018-11-26 16:43:22 -0500

    add celery version

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     author='Sam Kleiner',
     author_email='sam@skleiner.com',
     license='MIT',
-    install_requires=['redis~=2.10.6', 'celery', 'requests', 'beautifulsoup4'],
-    setup_requires=['redis~=2.10.6', 'celery', 'requests', 'beautifulsoup4'],
+    install_requires=[
+        'redis~=2.10.6', 'celery>=4.0.0', 'requests', 'beautifulsoup4'
+    ],
 )
